3ab57225c60dc523e4506e12f17040c68fa6b4fc
Apichat Wongtala <apichat26870@gmail.com>
2024-05-29 15:24:44 +0700

    update: file Login And Register in Components to commit

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Login.css";
+import "../../App.css";
 import { Link } from "react-router-dom";
 
 // import our assts
@@ -15,7 +16,7 @@ const Login = () => {
   return (
     <div className="loginPage flex">
       <div className="container flex">
-        {/* <div className="videoDiv">
+        <div className="videoDiv">
           <video src={video} autoPlay muted loop></video>
 
           <div className="textDiv">
@@ -29,7 +30,7 @@ const Login = () => {
               <button className="btn">Sign Up</button>
             </Link>
           </div>
-        </div> */}
+        </div>
 
         <div className="formDiv flex">
           <div className="headerDiv">
@@ -38,26 +39,39 @@ const Login = () => {
           </div>
 
           <form action="" className="form grid">
-            <span>Login Status will go here</span>
+            <span className="showMessage">Login Status will go here</span>
 
             <div className="inputDiv">
               <label htmlFor="username">Username</label>
               <div className="input flex">
-              <FaUserShield className="icon"/>
-              <input type="text" id="username" placeholder="Enter Username"/>
+                <FaUserShield className="icon" />
+                <input type="text" id="username" placeholder="Enter Username" />
               </div>
             </div>
 
             <div className="inputDiv">
               <label htmlFor="password">Password</label>
               <div className="input flex">
-              <BsFillShieldLockFill className="icon"/>
-              <input type="password" id="password" placeholder="Enter Password"/>
+                <BsFillShieldLockFill className="icon" />
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="Enter Password"
+                />
               </div>
             </div>
+
+            <button type="submit" className="btn flex">
+              <span>Login</span>
+              <AiOutlineSwapRight className="icon" />
+            </button>
+
+            <span className="forgotPassword">
+              Forgot your password? <a href="">Click here</a>
+            </span>
           </form>
         </div>
-
+        
       </div>
     </div>
   );
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,12 +1,78 @@
 import React from 'react'
 import './Register.css'
+import "../../App.css";
+import { Link } from "react-router-dom";
+
+// import our assts
+import video from "../../LoginAssets/video.mp4";
+import logo from "../../LoginAssets/logo.png";
+
+// Import Icons
+import { FaUserShield } from "react-icons/fa";
+import { BsFillShieldLockFill } from "react-icons/bs";
+import { AiOutlineSwapRight } from "react-icons/ai";
 
 const Register = () => {
   return (
-    <div>
-      This is Register page
-      <br />
-      <a href="/">To Login</a>
+    <div className="loginPage flex">
+      <div className="container flex">
+        <div className="videoDiv">
+          <video src={video} autoPlay muted loop></video>
+
+          <div className="textDiv">
+            <h2 className="title">Create And Sell Extraordinary Products</h2>
+            <p>Adopt the peace of nature!</p>
+          </div>
+
+          <div className="footerDiv flex">
+            <span className="text">Don't have an account?</span>
+            <Link to={"/"}>
+              <button className="btn">Sign Up</button>
+            </Link>
+          </div>
+        </div>
+
+        <div className="formDiv flex">
+          <div className="headerDiv">
+            <img src={logo} alt="Logo Image" />
+            <h3>Welcome Back!</h3>
+          </div>
+
+          <form action="" className="form grid">
+            <span className="showMessage">Login Status will go here</span>
+
+            <div className="inputDiv">
+              <label htmlFor="username">Username</label>
+              <div className="input flex">
+                <FaUserShield className="icon" />
+                <input type="text" id="username" placeholder="Enter Username" />
+              </div>
+            </div>
+
+            <div className="inputDiv">
+              <label htmlFor="password">Password</label>
+              <div className="input flex">
+                <BsFillShieldLockFill className="icon" />
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="Enter Password"
+                />
+              </div>
+            </div>
+
+            <button type="submit" className="btn flex">
+              <span>Register</span>
+              <AiOutlineSwapRight className="icon" />
+            </button>
+
+            <span className="forgotPassword">
+              Forgot your password? <a href="">Click here</a>
+            </span>
+          </form>
+        </div>
+        
+      </div>
     </div>
   )
 }
